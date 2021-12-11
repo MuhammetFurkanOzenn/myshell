75c17528d3a0d5be97ae42c0c76b67a68b99f277
Muhammet Furkan Özen <62333585+MuhammetFurkanOzenn@users.noreply.github.com>
2021-12-11 21:53:23 +0300

    add some command

--- myshell.c
+++ myshell.c
@@ -1,21 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main () {	
+int main () {	//(int argc, char *argv[]){
 	printf("hello world!\n");
 	int i = 0;
 	int loop = 1; 
-	char * command;
+	//char * command;
+	char command[40];
 	
+	char command_p1[40]; // tekrar
+	char command_p2[40]; // sultan
+	char command_p3[40]; // 2
+	char command_p4[40]; // null | sayi
+	
+	
+	char _exit[]={'e','x','i','t','\0'};
+	char _bash[]={'b','a','s','h','\0'};
+	char _tekrar[]={'t','e','k','r','a','r','\0'};
+	
+        printf("%s %s\n",_exit, _bash);
+        int id;
+	// while 
 	while (loop == 1){
-		i++;
+		
 		printf("myshell>> ");
 		scanf("%s",command);
 		printf("%s \n",command);
-		if (i == 10){
+		
+		if(strcmp(command, _bash) == 0){
+			printf("Bash'a geciliyor...\n");
+		}
+		else if(strcmp(command, _tekrar) == 0){
+			
+			printf("Tekrar'a geciliyor...\n");
+		}
+			
+		else if (strcmp(command, _exit) == 0){
 			break;
 		}
+		else
+			printf("Yanlis bir komut girdiniz...\nKomut listesi icin /help yazabilirsiniz...");
 		
-	}	
+	}
 	
+		
    return 0;
 }
