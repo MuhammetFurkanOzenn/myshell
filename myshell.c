3415c47f601ee13b9a8d0ea144d78297ffa2fc17
Muhammet Furkan Özen <62333585+MuhammetFurkanOzenn@users.noreply.github.com>
2021-12-12 03:22:31 +0300

    add split functionality to myshell.c

--- myshell.c
+++ myshell.c
@@ -1,47 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main () {	//(int argc, char *argv[]){
-	printf("hello world!\n");
+	
 	int i = 0;
 	int loop = 1; 
-	//char * command;
+	int id;
+        int frk;
+	
 	char command[40];
-	
-	char command_p1[40]; // tekrar
-	char command_p2[40]; // sultan
-	char command_p3[40]; // 2
-	char command_p4[40]; // null | sayi
-	
-	
 	char _exit[]={'e','x','i','t','\0'};
 	char _bash[]={'b','a','s','h','\0'};
 	char _tekrar[]={'t','e','k','r','a','r','\0'};
 	
-        printf("%s %s\n",_exit, _bash);
-        int id;
-	// while 
-	while (loop == 1){
+	
+    	char * split;
+    	char * word;
+    	char * command_p1;
+    	char * command_p2;
+    	char * command_p3;
+    	char * command_p4;
+
+	//printf("%s %s\n",_exit, _bash);
+        
+	//while (loop == 1){
 		
 		printf("myshell>> ");
-		scanf("%s",command);
-		printf("%s \n",command);
+		scanf("%29[^\n]",command);
+		fflush(stdin);
+		fflush(stdout);
+        	printf("command :: %s\n" , command);
 		
-		if(strcmp(command, _bash) == 0){
+		// word 1
+	    	split=strtok(command," ");
+		command_p1 = split;
+		printf("pp : %s \n", command_p1);
+		
+		// word 2
+	    	word=split;
+	    	split=strtok(NULL," ");
+	    	command_p2 = split;
+		printf("pp : %s \n", command_p2);
+	    		
+	    	// word 3	
+	    	word=split;
+	    	split=strtok(NULL," ");
+		command_p3 = split;
+		printf("pp : %s \n", command_p3);
+		
+		// word 4	
+	    	word=split;
+	    	split=strtok(NULL," ");
+		command_p4 = split;
+		printf("pp : %s \n", command_p4);
+		
+		if (command_p4 == NULL)
+			printf(" p4 is NULL\n");
+			
+		if(strcmp(command_p1, _bash) == 0 && command_p2 == NULL){
 			printf("Bash'a geciliyor...\n");
+				
+			/*frk = fork();
+			if (frk == 0){
+				printf("child pid : %d\n",getppid());
+				sleep(5);
+				printf("child pid after sleep: %d\n",getppid());	
+			}
+			else{
+				printf("ppid : %d\n",getpid());
+			}*/	
 		}
-		else if(strcmp(command, _tekrar) == 0){
+		
+		else if(strcmp(command_p1, _tekrar) == 0){
 			
 			printf("Tekrar'a geciliyor...\n");
 		}
 			
-		else if (strcmp(command, _exit) == 0){
-			break;
+		else if (strcmp(command_p1, _exit) == 0){
+			//break;
 		}
 		else
-			printf("Yanlis bir komut girdiniz...\nKomut listesi icin /help yazabilirsiniz...");
+			printf("Yanlis bir komut girdiniz...\nKomut listesi icin /help yazabilirsiniz...\n");
 		
-	}
+	//}
 	
 		
    return 0;
